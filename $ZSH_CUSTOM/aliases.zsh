8efbc82229429551946a49707aa21916ca69be60
Luiza Primo <99497736+primo1851@users.noreply.github.com>
2024-10-21 11:31:17 -0300

    Create aliases.zsh

---
+++ $ZSH_CUSTOM/aliases.zsh
@@ -0,0 +1,2 @@
+alias programmer="sudo /usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI $1"
+alias pycharm="/mnt/c/Program\ Files/JetBrains/PyCharm\ 2024.2.3/bin/pycharm64.exe $1 && exit"
